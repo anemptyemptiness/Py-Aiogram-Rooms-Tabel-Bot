f92f7d8c848b4effbe68fe232323200293e8509c
anemptyemptiness <103761193+anemptyemptiness@users.noreply.github.com>
2024-02-12 17:57:13 +0300

    add employee

--- config/config.py
+++ config/config.py
@@ -28,7 +28,7 @@ def load_config() -> Config:
     env.read_env()
 
     return Config(tg_bot=TgBot(token=env("TOKEN")),
-                  employees=[int(f"{env(f'employee_{i}')}") for i in range(1, 22)],
+                  employees=[int(f"{env(f'employee_{i}')}") for i in range(1, 23)],
                   admins=[int(f"{env(f'admin_{i}')}") for i in range(1, 4)],
                   admin_chats=[f"{env(f'chat_id_{i}')}" for i in range(1, 11)],
                   places=[f"{env(f'place_{i}')}" for i in range(1, 11)],
